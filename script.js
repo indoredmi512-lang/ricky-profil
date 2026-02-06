fff9707dfbb7192f5bf2fabfa07b75b13ef50736
Ricky <indoredmi512@gmail.com>
2026-02-06 13:11:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+const songs = [
+"musik1.mp3",
+"musik2.mp3",
+"musik3.mp3"
+];
 
+let index = 0;
+const audio = document.getElementById("audio");
+const title = document.getElementById("songTitle");
+
+function load(){
+audio.src = songs[index];
+title.innerHTML = songs[index];
+}
+
+function play(){
+audio.play();
+}
+
+function next(){
+index = (index + 1) % songs.length;
+load();
+audio.play();
+}
+
+function prev(){
+index = (index - 1 + songs.length) % songs.length;
+load();
+audio.play();
+}
+
+load();
+</script>
